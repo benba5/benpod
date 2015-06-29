9524a7aba1fc85e6d0ec6af8aabec3b41e774d94
Ben <ben@appsynth.net>
2015-06-29 17:22:03 +0700

    Initial pod project

---
+++ PodLibrarySample/PodLibrarySample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodLibrarySample
+//
+//  Created by appsynth on 6/29/15.
+//  Copyright (c) 2015 Appsynth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodLibrarySample/PodLibrarySample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodLibrarySample
+//
+//  Created by appsynth on 6/29/15.
+//  Copyright (c) 2015 Appsynth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

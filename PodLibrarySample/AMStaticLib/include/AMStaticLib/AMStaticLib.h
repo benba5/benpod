9f7b195a75c66a0ebe1b31708dce97429d21646e
Ben <ben@appsynth.net>
2015-06-29 20:58:25 +0700

    Add AMStaticLib

---
+++ PodLibrarySample/AMStaticLib/include/AMStaticLib/AMStaticLib.h
@@ -0,0 +1,13 @@
+//
+//  AMStaticLib.h
+//  AMStaticLib
+//
+//  Created by appsynth on 6/26/15.
+//  Copyright (c) 2015 Appsynth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMStaticLib : NSObject
+
+@end
